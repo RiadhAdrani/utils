9b8e967276450d8badabc6457081daf80e37521d
RiadhAdrani <riadh_adrani@hotmail.fr>
2022-10-14 19:45:28 +0200

    feat: added range

---
+++ src/range.js
@@ -0,0 +1,24 @@
+/**
+ * generate an array containing the numbers in `start..end`.
+ * @param {number} end ending number, this value is included.
+ * @param {number} start starting point. 0 by default.
+ * @throw when start > end.
+ * @returns {Array<number>}
+ */
+function range(end, start = 0) {
+    if (typeof end != "number" || typeof start != "number") return;
+
+    if (start > end) {
+        throw new Error("start number is greater than the end one.");
+    }
+
+    const output = [];
+
+    for (let i = start; i <= end; i++) {
+        output.push(i);
+    }
+
+    return output;
+}
+
+module.exports = range;
