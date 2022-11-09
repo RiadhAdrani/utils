57caa833ff35689e4684a4a27df170c8c792c995
RiadhAdrani <riadh_adrani@hotmail.fr>
2022-11-09 21:48:52 +0100

    fix: updated getColorType

--- src/getColorType/index.js
+++ src/getColorType/index.js
@@ -1,6 +1,6 @@
 const isHexColor = require("../isHexColor");
-const isHslColor = require("../isHslColor");
-const isRgbColor = require("../isRgbColor");
+const { isHslColor } = require("../isHslColor");
+const { isRgbColor } = require("../isRgbColor");
 
 const HEX = "hex";
 const HSL = "hsl";
@@ -8,7 +8,7 @@ const RGB = "rgb";
 const UNKNOWN = "unknown";
 
 const getColorType = (color) => {
-    return isHexColor(color) ? HEX : isHslColor(color) ? HSL : isRgbColor(color) ? RGB : UNKNOWN;
+  return isHexColor(color) ? HEX : isHslColor(color) ? HSL : isRgbColor(color) ? RGB : UNKNOWN;
 };
 
-module.exports = { getColorType, HEX, HSL, RGB };
+module.exports = { getColorType, HEX, HSL, RGB, UNKNOWN };
--- src/getColorType/index.test.js
+++ src/getColorType/index.test.js
@@ -1,17 +1,17 @@
 const { getColorType } = require(".");
 
 describe("getColorType", () => {
-    it.each([
-        ["red", "unknown"],
-        [123, "unknown"],
-        ["rgb(1,2,3)", "rgb"],
-        ["rgba(1,2,3,0.5)", "rgb"],
-        ["hsl(1,2,3)", "hsl"],
-        ["hsla(1,2,3,0.5)", "hsl"],
-        ["#000", "hex"],
-        ["#111111", "hex"],
-        ["#aaaaaaaa", "hex"],
-    ])('should return the correct type "%s"', (color, expected) => {
-        expect(getColorType(color)).toBe(expected);
-    });
+  it.each([
+    ["red", "unknown"],
+    [123, "unknown"],
+    ["rgb(1,2,3)", "rgb"],
+    ["rgba(1,2,3,0.5)", "rgb"],
+    ["hsl(1deg 2% 3%)", "hsl"],
+    ["hsla(360deg 2% 3% / 0.5)", "hsl"],
+    ["#000", "hex"],
+    ["#111111", "hex"],
+    ["#aaaaaaaa", "hex"],
+  ])('should return the correct type "%s"', (color, expected) => {
+    expect(getColorType(color)).toBe(expected);
+  });
 });
