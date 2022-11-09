385a9e66b47fd488789b140c528bca3b19fd1af7
RiadhAdrani <riadh_adrani@hotmail.fr>
2022-11-09 21:17:23 +0100

    fix: fixed isHslColor not accepting floating numbers

--- src/isHslColor/index.js
+++ src/isHslColor/index.js
@@ -1,48 +1,81 @@
 const isBlank = require("../isBlank");
 
-/**
- * @param {string} color
- */
-module.exports = (color) => {
-    if (isBlank(color)) {
-        return false;
-    }
+const HSL_REGEX = /^hsl\((\d{1,3})(\.\d+){0,1}deg (\d{1,3})(\.\d+){0,1}% (\d{1,3})(\.\d+){0,1}%\)$/;
+const HSLA_REGEX =
+  /^hsla\((\d{1,3})(\.\d+){0,1}deg (\d{1,3})(\.\d+){0,1}% (\d{1,3})(\.\d+){0,1}%( \/ (\d{1,3})(\.\d+){0,1}){0,1}\)$/;
+const isHslForm = (color) => {
+  return isBlank(color) ? false : HSL_REGEX.test(color);
+};
 
-    const hslRegEx = /^hsl\((\d{1,3}),( ){0,1}(\d{1,3}),( ){0,1}(\d{1,3})\)$/;
-    const hslaRegEx =
-        /^hsla\((\d{1,3}),( ){0,1}(\d{1,3}),( ){0,1}(\d{1,3})(,( ){0,1}(\d{1})(\.\d+){0,1}){0,1}\)$/;
+const isHslaForm = (color) => {
+  return isBlank(color) ? false : HSLA_REGEX.test(color);
+};
 
-    if (hslRegEx.test(color)) {
-        const data = color
-            .slice(4, -1)
-            .split(",")
-            .map((i) => parseInt(i.trim()));
+const extractDataFromHSL = (color) => {
+  if (isHslaForm(color)) {
+    return color
+      .slice(5, -1)
+      .replace("%", "")
+      .replace("deg", "")
+      .replace("/", "")
+      .replace("  ", " ")
+      .split(" ")
+      .map((i) => parseFloat(i.trim()));
+  }
 
-        for (let n of data) {
-            if (n > 256) return false;
-        }
+  if (isHslForm(color)) {
+    return color
+      .slice(4, -1)
+      .replace("%", "")
+      .replace("deg", "")
+      .replace("/", "")
+      .split(" ")
+      .map((i) => parseFloat(i.trim()));
+  }
 
-        return true;
-    } else if (hslaRegEx.test(color)) {
-        const data = color
-            .slice(5, -1)
-            .split(",")
-            .map((i) => parseInt(i.trim()));
+  throw "(color) is not of a HSL/HSLA form.";
+};
 
-        const h = data.slice(0, 3)[0];
-        const s = data.slice(0, 3)[1];
-        const l = data.slice(0, 3)[2];
-
-        if (360 < h || h < 0) return false;
-        if (100 < s || s < 0) return false;
-        if (100 < l || l < 0) return false;
-
-        if (data[3] !== undefined && (1 < data[3] || data[3] < 0)) {
-            return false;
-        }
-
-        return true;
-    }
-
+const isHslColor = (color) => {
+  if (isBlank(color)) {
     return false;
+  }
+
+  const hslRegEx =
+    /^hsl\((\d{1,3})(\.\d+){0,1}deg (\d{1,3})(\.\d+){0,1}% (\d{1,3})(\.\d+){0,1}%\)$/;
+  const hslaRegEx =
+    /^hsla\((\d{1,3})(\.\d+){0,1}deg (\d{1,3})(\.\d+){0,1}% (\d{1,3})(\.\d+){0,1}%( \/ (\d{1,3})(\.\d+){0,1}){0,1}\)$/;
+
+  if (hslRegEx.test(color)) {
+    const [h, s, l] = extractDataFromHSL(color);
+
+    if (360 < h || h < 0) return false;
+    if (100 < s || s < 0) return false;
+    if (100 < l || l < 0) return false;
+
+    return true;
+  } else if (hslaRegEx.test(color)) {
+    const [h, s, l, a] = extractDataFromHSL(color);
+
+    if (360 < h || h < 0) return false;
+    if (100 < s || s < 0) return false;
+    if (100 < l || l < 0) return false;
+
+    if (a !== undefined && (1 < a || a < 0)) {
+      return false;
+    }
+
+    return true;
+  }
+
+  return false;
+};
+
+module.exports = {
+  HSLA_REGEX,
+  HSL_REGEX,
+  isHslForm,
+  isHslaForm,
+  extractDataFromHSL,
+  isHslColor,
 };
--- src/isHslColor/index.test.js
+++ src/isHslColor/index.test.js
@@ -1,107 +1,110 @@
-const isHslColor = require(".");
+const { isHslColor, isHslForm, isHslaForm, extractDataFromHSL } = require(".");
 
-describe("ishslColor", () => {
-    it.each([[1], [[]], [{}], [null], [undefined]])("should refuse non string values", (value) => {
-        expect(isHslColor(value)).toBe(false);
-    });
+describe("isHSLColor", () => {
+  it.each([[1], [[]], [{}], [null], [undefined]])("should refuse non string values", (value) => {
+    expect(isHslColor(value)).toBe(false);
+  });
 
-    it.each([
-        ["hsl (255,255,255)"],
-        ["hsla (255,255,255)"],
+  it.each([
+    ["", false],
+    ["hsl(10deg 5% 10% 1)", false],
+    ["hsl(10deg % 10% 1)", false],
+    ["hsl(deg 5% 10% 1)", false],
+    ["hsl(10deg 5% % 1)", false],
+    ["hsl(3000deg 100% 100%)", false],
+    ["hsl(300deg 1000% 100%)", false],
+    ["hsl(300deg 100% 1000%)", false],
+    ["hsl(10deg 5% 10%)", true],
+    ["hsl(10.5deg 5.5% 10.5%)", true],
+  ])("should detect if a string is a hsl form : %s", (color, expected) => {
+    expect(isHslForm(color)).toBe(expected);
+  });
 
-        ["hsl( 0,255,255)"],
-        ["hsla( 0,255,255)"],
+  it.each([
+    ["", false],
+    ["hsl(10deg 5% 10% 1)", false],
+    ["hsl(10deg % 10% 1)", false],
+    ["hsl(deg 5% 10% 1)", false],
+    ["hsl(10deg 5% % 1)", false],
+    ["hsl(3000deg 100% 100%)", false],
+    ["hsl(300deg 1000% 100%)", false],
+    ["hsl(300deg 100% 1000%)", false],
+    ["hsl(10deg 5% 10%)", true],
+    ["hsl(10.5deg 5.5% 10.5%)", true],
+  ])("should extract data from hsl/hsla : %s", (color, expected) => {
+    expect(isHslForm(color)).toBe(expected);
+  });
 
-        ["hsl(-1,255,255)"],
-        ["hsla(-1,255,255)"],
+  it.each([
+    ["hsla(10deg 5% 10% / 1)", [10, 5, 10, 1]],
+    ["hsl(10deg 2.55% 10%)", [10, 2.55, 10]],
+  ])("should detect if a string is a hsla form : %s", (color, expected) => {
+    expect(extractDataFromHSL(color)).toStrictEqual(expected);
+  });
 
-        ["hsl(361,0,0)"],
-        ["hsla(361,0,0)"],
+  it("should throw an error when the hsl form is not detected", () => {
+    expect(() => extractDataFromHSL("hsl()")).toThrow();
+  });
 
-        ["hsl(255,101 ,255)"],
-        ["hsla(255,101 ,255)"],
+  it.each([
+    ["hsl (255 255 255)"],
+    ["hsla (255 255 255)"],
 
-        ["hsl(255,-1,255)"],
-        ["hsla(255,-1,255)"],
+    ["hsl( 0 255 255)"],
+    ["hsla( 0 255 255)"],
 
-        ["hsl(255,257,255)"],
-        ["hsla(255,257,255)"],
+    ["hsl(-1 255 255)"],
+    ["hsla(-1 255 255)"],
 
-        ["hsl(255,255,255 )"],
-        ["hsla(255,255,255 )"],
+    ["hsl(361 0 0)"],
+    ["hsla(361 0 0)"],
 
-        ["hsl(255,255,-1)"],
-        ["hsla(255,255,-1)"],
+    ["hsl(255 101  255)"],
+    ["hsla(255,101  255)"],
 
-        ["hsl(255,255,257)"],
-        ["hsla(255,255,257)"],
+    ["hsl(255 -1 255)"],
+    ["hsla(255 -1 255)"],
 
-        ["hsla(255,255,257,1 )"],
-        ["hsla(255,255,257,1.1)"],
-        ["hsla(255,255,257,-0.1)"],
-    ])("should refuse bad forms '%s'", (value) => {
-        expect(isHslColor(value)).toBe(false);
-    });
+    ["hsl(255 257 255)"],
+    ["hsla(255 257 255)"],
 
-    it.each([
-        ["hsl(0,0,0)"],
-        ["hsl(0, 0,0)"],
-        ["hsl(0, 0, 0)"],
-        ["hsl(0,0, 0)"],
+    ["hsl(255 255 255 )"],
+    ["hsla(255 255 255 )"],
 
-        ["hsl(1,1,1)"],
-        ["hsl(1, 1,1)"],
-        ["hsl(1, 1, 1)"],
-        ["hsl(1,1, 1)"],
+    ["hsl(255 255 -1)"],
+    ["hsla(255 255 -1)"],
 
-        ["hsl(10,10,10)"],
-        ["hsl(10, 10,10)"],
-        ["hsl(10, 10, 10)"],
-        ["hsl(10,10, 10)"],
+    ["hsl(255 255 257)"],
+    ["hsla(255 255 257)"],
 
-        ["hsl(100,100,100)"],
-        ["hsl(100, 100,100)"],
-        ["hsl(100, 100, 100)"],
-        ["hsl(100,100, 100)"],
+    ["hsla(255 255 257 1 )"],
+    ["hsla(255 255 257 1.1)"],
+    ["hsla(255 255 257 -0.1)"],
+  ])("should refuse bad forms '%s'", (value) => {
+    expect(isHslColor(value)).toBe(false);
+  });
 
-        ["hsl(256,50,33)"],
-        ["hsl(256, 50,44)"],
-        ["hsl(256, 50, 99)"],
-        ["hsl(256,50, 0)"],
+  it.each([
+    ["hsl(10.5deg 10% 10%)"],
+    ["hsl(10.235deg 10% 10%)"],
+    ["hsl(100.89deg 10% 10%)"],
+    ["hsl(360deg 10% 10%)"],
 
-        ["hsla(0,0,0)"],
-        ["hsla(0, 0,0)"],
-        ["hsla(0, 0, 0)"],
-        ["hsla(0,0, 0)"],
+    ["hsla(100deg 100% 100%)"],
 
-        ["hsla(1,1,1)"],
-        ["hsla(1, 1,1)"],
-        ["hsla(1, 1, 1)"],
-        ["hsla(1,1, 1)"],
+    ["hsla(360deg 75% 50%)"],
+    ["hsla(256deg 75% 50%)"],
+    ["hsla(256deg 30% 50%)"],
+    ["hsla(360deg 100% 0%)"],
 
-        ["hsla(10,10,10)"],
-        ["hsla(10, 10,10)"],
-        ["hsla(10, 10, 10)"],
-        ["hsla(10,10, 10)"],
-
-        ["hsla(100,100,100)"],
-        ["hsla(100, 100,100)"],
-        ["hsla(100, 100, 100)"],
-        ["hsla(100,100, 100)"],
-
-        ["hsla(360,75,50)"],
-        ["hsla(256, 75,50)"],
-        ["hsla(256, 30, 50)"],
-        ["hsla(360,100, 0)"],
-
-        ["hsla(0,10,55,1)"],
-        ["hsla(360,25,25,0)"],
-        ["hsla(360,25,25, 1)"],
-        ["hsla(360,25,25, 0)"],
-        ["hsla(360,25,25,0.1)"],
-        ["hsla(360,25,25,0.105975)"],
-        ["hsla(360,25,25, 0.254725)"],
-    ])("should accept good forms '%s'", (value) => {
-        expect(isHslColor(value)).toBe(true);
-    });
+    ["hsla(0deg 10% 55% / 1)"],
+    ["hsla(360deg 25% 25% / 0)"],
+    ["hsla(360deg 25% 25% / 1)"],
+    ["hsla(360deg 25% 25% / 0)"],
+    ["hsla(360deg 25% 25% / 0.1)"],
+    ["hsla(360deg 25% 25% / 0.105975)"],
+    ["hsla(360deg 25% 25% / 0.254725)"],
+  ])("should accept good forms '%s'", (value) => {
+    expect(isHslColor(value)).toBe(true);
+  });
 });
