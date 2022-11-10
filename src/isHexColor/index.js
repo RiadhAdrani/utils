8ded3f7ac182293da76c70cbd13d0b5b829b3b7d
RiadhAdrani <riadh_adrani@hotmail.fr>
2022-11-10 19:23:28 +0100

    fix: uppercase allowed in HEX colors

--- src/isHexColor/index.js
+++ src/isHexColor/index.js
@@ -6,13 +6,13 @@ const isBlank = require("../isBlank");
  * @returns
  */
 function isHexColor(color) {
-    if (isBlank(color)) return false;
+  if (isBlank(color)) return false;
 
-    const three = /^#[0-9|a|b|c|d|e|f]{3}$/;
-    const six = /^#[0-9|a|b|c|d|e|f]{6}$/;
-    const eight = /^#[0-9|a|b|c|d|e|f]{8}$/;
+  const three = /^#[0-9|a|A|b|B|c|C|d|D|e|E|f|F]{3}$/;
+  const six = /^#[0-9|a|A|b|B|c|C|d|D|e|E|f|F]{6}$/;
+  const eight = /^#[0-9|a|A|b|B|c|C|d|D|e|E|f|F]{8}$/;
 
-    return three.test(color) || six.test(color) || eight.test(color);
+  return three.test(color) || six.test(color) || eight.test(color);
 }
 
 module.exports = isHexColor;
--- src/isHexColor/index.test.js
+++ src/isHexColor/index.test.js
@@ -1,21 +1,25 @@
 const isHexColor = require(".");
 
 test.each([
-    [1, false],
-    ["", false],
-    ["#", false],
-    ["#1", false],
-    ["#12", false],
-    ["#1 2", false],
-    ["#1g2", false],
-    ["#abc", true],
-    ["#132", true],
-    ["#1a3", true],
-    ["#1a32", false],
-    ["#1a323", false],
-    ["#1a323a", true],
-    ["#1a323a3", false],
-    ["#1a323acc", true],
+  [1, false],
+  ["", false],
+  ["#", false],
+  ["#1", false],
+  ["#12", false],
+  ["#1 2", false],
+  ["#1g2", false],
+  ["#abc", true],
+  ["#aBc", true],
+  ["#132", true],
+  ["#1a3", true],
+  ["#1A3", true],
+  ["#1a32", false],
+  ["#1a323", false],
+  ["#1a323a", true],
+  ["#1a323A", true],
+  ["#1a323a3", false],
+  ["#1a323acc", true],
+  ["#1A323ACC", true],
 ])("should determine if '%s' is a hex color value, expected : '%s'", (input, expected) => {
-    expect(isHexColor(input)).toBe(expected);
+  expect(isHexColor(input)).toBe(expected);
 });
