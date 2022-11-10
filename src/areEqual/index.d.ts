31b9189249777025869b24e40b1d55463ccc8ec6
RiadhAdrani <riadh_adrani@hotmail.fr>
2022-11-10 19:56:50 +0100

    refactor: initialized ts configs

--- src/areEqual/index.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * perform deep comparison of two objects of any type.
- * @param obj1 first object.
- * @param obj2 second object.
- * @param depth maximum comparison depth, `10` by default.
- * This value improve performance with large object and circular dependencies
- */
-export default function areEqual<T, K>(obj1: T, obj2: K, depth: number): boolean;
